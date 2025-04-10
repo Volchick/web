3561f7269e2e9796179b679bd0b7baa0e3bd5b60
Jize <meshcheryakov20122003@gmail.com>
2025-04-10 23:23:58 +0300

    web4

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,50 @@
 import express, { Request, Response } from "express"
+import mongoose from "mongoose"
+import User from "./models/User"
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
 
-app.set("view engine", "ejs")
+mongoose
+    .connect("mongodb://admin:1234@localhost:27017/web?authSource=admin")
+    .then(() => console.log("DB connected!"))
+    .catch((err) => console.error(err))
 
-app.get('/ejs-pages', (req: Request, res: Response) => {
-    res.render('layout', { title: "ejs-pages", message: "Hi, user!", content: "index", content2: "about" })
+// Получение всех пользователей
+app.get("/users", async (req: Request, res: Response) => {
+    try {
+        const users = await User.find();
+        res.status(201).json(users);
+    } catch (e) {
+        res.status(400).json({ message: (e as Error).message })
+    }
 })
 
-app.get('/about', (req: Request, res: Response) => {
-    res.render('layout', { title: "about", content: "about" })
+// Добавление пользователя
+app.post("/users", async (req: Request, res: Response) => {
+    try {
+        console.log(req.body);
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json(user);
+    } catch (e) {
+        res.status(400).json({ message: (e as Error).message })
+    }
 })
 
-app.get('/', (req: Request, res: Response) => {
-    res.render('layout', { title: "Main", content: "main" })
+// Удаление пользователя 
+app.delete("/users", async (req: Request, res: Response) => {
+    try {
+        console.log(req.body);
+        const result = await User.deleteOne(req.body);
+        res.status(201).json(result);
+    } catch (e) {
+        res.status(400).json({ message: (e as Error).message })
+    }
 })
 
-app.post('/', (req: Request, res: Response) => {
-    console.log(req.body)
-    console.log(req.headers)
-    res.send('Hello World!')
-})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
